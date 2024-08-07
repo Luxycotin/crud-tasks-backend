313c457d0cf11007a8b46f401f27b433a3612f18
Luca Espinoza <luca.g.esp2005@gmail.com>
2024-08-07 14:44:10 -0300

    Se termina el proceso de conexiones

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # crud-tasks-backend
+Un trabajo de diagnóstio en el que se trabajará en un Crud, usando mysql, node, express.
+Este trabajo es el "Reinicio de uno anterior" el cuál se me complicó seguir a la hora de no comprender un error a la hora de actualizar los datos de los registros, entonces decidí hacer todo de nuevo
--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import express from 'express'
+import { engine }  from 'express-handlebars';
+import morgan from 'morgan';
+import {join, dirname} from 'path'
+import { fileURLToPath } from 'url';
 
 
-const app = express();;;;
+
+const app = express();
+const _dirname = dirname(fileURLToPath(import.meta.url));
+
+
 
 app.set('port', process.env.PORT || 3000)
+app.set('views', join=(__dirname, 'views') )
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'),'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),
+    extreme: '.hbs'
+}
+))
+app.set('view engine', '.hbs');
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 
-app.set('views', )
-
-
+SVGClipPathElement.get('/', (req, res)=>(
+    res.json({"message": "hola :3"})
+))
 
 app.listen(app.get('port'), ()=>
 console.log('servidor escuchando desde el puerto', app.get('port'))
