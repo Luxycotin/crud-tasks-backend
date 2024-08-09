b117b82d2283012dec8bd448eb2e42fb9f1189f0
LucaEspinoza <luca.g.esp2005@gmail.com>
2024-08-08 23:45:36 -0300

    Se finalizó el trabajo

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # crud-tasks-backend
-Un trabajo de diagnóstio en el que se trabajará en un Crud, usando mysql, node, express.
-Este trabajo es el "Reinicio de uno anterior" el cuál se me complicó seguir a la hora de no comprender un error a la hora de actualizar los datos de los registros, entonces decidí hacer todo de nuevo
+#Este es un trabajo de diagnostico, donde se realiza un CRUD con validaciones, y ademas se usa un servidor mysql donde se guarda toda la información
--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
-import express from 'express'
-import { engine }  from 'express-handlebars';
-import morgan from 'morgan';
-import {join, dirname} from 'path'
-import { fileURLToPath } from 'url';
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
 
+const app = express()
 
+//estos son los midellwares
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(require("./router"))
 
-const app = express();
-const _dirname = dirname(fileURLToPath(import.meta.url));
+app.listen(3000, ()=> {console.log("El servidor corriendose con éxito en el puerto 3000")});
 
-
-
-app.set('port', process.env.PORT || 3000)
-app.set('views', join=(__dirname, 'views') )
-app.engine('.hbs', engine({
-    defaultLayout: 'main',
-    layoutsDir: join(app.get('views'),'layouts'),
-    partialsDir: join(app.get('views'), 'partials'),
-    extreme: '.hbs'
-}
-))
-app.set('view engine', '.hbs');
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: false}));
-app.use(express.json());
-
-SVGClipPathElement.get('/', (req, res)=>(
-    res.json({"message": "hola :3"})
-))
-
-app.listen(app.get('port'), ()=>
-console.log('servidor escuchando desde el puerto', app.get('port'))
-);
